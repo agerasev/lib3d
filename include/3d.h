0620013bbdd773022efdbaa086946a4f1dcc6612
Alex <alexey.gerasev@gmail.com>
2015-11-24 16:22:32 +0600

    build script

---
+++ include/3d.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int lib3d_init();
+void lib3d_dispose();
+
+#ifdef __cplusplus
+}
+#endif
---
+++ source/main.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+int main(int argc, char *argv[]) {
+	printf("Hello, lib3d!\n");
+	return 0;
+}
